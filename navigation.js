b3f5679d44ee3f4f61b9b407c158744ca2b1776d
yuuki008 <s22ah261@seinan-gakuin.jp>
2021-11-01 02:38:53 +0900

    delete: オーダー完了画面

--- navigation.js
+++ navigation.js
@@ -3,6 +3,7 @@ import { createStackNavigator } from '@react-navigation/stack'
 import { NavigationContainer } from '@react-navigation/native'
 import Home from './screens/Home'
 import RestaurantDetail from './screens/RestaurantDetail'
+import OrderCompleted from './screens/OrderCompleted'
 import { Provider as ReduxProvider } from "react-redux";
 import configureStore from "./redux/store";
 
@@ -21,6 +22,7 @@ export default function RootNavigation() {
         <Stack.Navigator initialRouteName="Home" screenOptions={screenOptions}>
           <Stack.Screen name="Home" component={Home} />
           <Stack.Screen name="RestaurantDetail" component={RestaurantDetail} />
+          <Stack.Screen name="OrderCompleted" component={OrderCompleted} />
         </Stack.Navigator>
       </NavigationContainer>
     </ReduxProvider>
---
+++ screens/OrderCompleted.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function OrderCompleted (){
+  return (
+    <div>
+      Hello World!
+    </div>
+  )
+}
+
